764a41ef25067eb5f9d9a74a84bb465f48565772
patl65 <patlay65@gmail.com>
2021-05-31 16:26:02 +0200

    mise en page + texte : page console

--- resources/views/pages/admin/console.blade.php
+++ resources/views/pages/admin/console.blade.php
@@ -1,8 +1,19 @@
 {{-- @extends('layout.layoutAdmin') --}}
 @extends('layout.layoutAdmin')
 @section('content')
-<h1>Administration du site du projet 2021</h1>
-<h1>Titre développeuse web et web mobile</h1>
-<h1>Patricia Layerle</h1>
-
+    <div class="d-flex justify-content-center mt-5">
+        <div class="card bg-secondary text-primary border-primary rounded-pill shadow-lg p-2">
+            <div class="card-body">
+                <center><i>
+                        <h2>Administration du site projet 2021</h2>
+                    </i></center>
+                <center><i>
+                        <h2>Titre développeuse web et web mobile</h2>
+                    </i></center>
+                <center><i>
+                        <h2>Patricia Layerle</h2>
+                    </i></center>
+            </div>
+        </div>
+    </div>
 @endsection
