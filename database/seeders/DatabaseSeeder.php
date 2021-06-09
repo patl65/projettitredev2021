49d3306c7e0ab920c99311ab6029bd99d5c051d2
patl65 <patlay65@gmail.com>
2021-06-09 20:13:05 +0200

    modification commentaire + anglais

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,30 +21,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // //pour la mise en production : crée un utilisateur qu'il va chercher dans .env
-        // User::create([
-        //     'firstName' => config('settings.credentials.firstname'),
-        //     'lastName' => config('settings.credentials.lastname'),
-        //     'userName' => config('settings.credentials.username'),
-        //     'email' => config('settings.credentials.email'),
-        //     'password' => Hash::make(config('settings.credentials.password')),
-        //     'email_verified_at' => now(),
-        //     'phoneNumber' => config('settings.credentials.phoneNumber'),
-        //     'address' => config('settings.credentials.address'),
-        //     'postcode' => config('settings.credentials.postcode'),
-        //     'city' => config('settings.credentials.city'),
-        //     'country' => config('settings.credentials.country'),
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        //     'administrator' => true,
-        //     'gtc' => true
-        // ]);
-
-        //le if permet de sécuriser les données de la BDD quand elle est en prod
-
-        // Ligne pour changement PC et réup BDD avec des données tests
+        // Ligne pour changement PC et récup BDD avec des données tests
         // if(app()->environment('local') || app()->environment('install')) {
         // Ligne pour lancement site avec une BDD avec champs vides
+
+        //le if permet de sécuriser les données de la BDD quand elle est en prod
+        //if: it's for securty datas of database when it will be in prod
         if (app()->environment('local')) {
             $this->call([
                 UserSeeder::class,
